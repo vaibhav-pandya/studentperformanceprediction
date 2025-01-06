97932c21a21df0d3ba7384d4aead79239240ff39
vaibhav-pandya <vaibhavpandya2903@gmail.com>
2025-01-06 17:00:28 +0530

    Second commit - setup

---
+++ setup.py
@@ -0,0 +1,31 @@
+#It will responsible in creating my ML application as a package, which can be used in a projects
+#even it can be deploy in PyPi(Python Package Index) and from there anybody can do the installation and anybody can use it.
+
+from setuptools import find_packages, setup #find all the packages available in ML application
+from typing import List
+
+HYPEN_E_DOT= '-e .'
+def get_requirements(file_path: str)->List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n", "") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return file_path
+
+
+setup(
+name='MLProject',
+version='0.0.1', #my first version
+author='Vaibhav',
+author_email='vaibhavpandya2903@gmail.com',
+packages=find_packages(), #it will go and see how many folders have __init__.py files like in src folder
+install_requires=get_requirements('requirements.txt')
+)
