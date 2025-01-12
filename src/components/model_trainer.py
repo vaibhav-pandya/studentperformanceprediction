9c14bb20d9ae0c992b83cc0ebc77e1f2e1d3b66f
vaibhav-pandya <vaibhavpandya2903@gmail.com>
2025-01-12 14:04:26 +0530

    Hyperparameter training

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -46,12 +46,48 @@ class ModelTrainer:
                 'Gradient Boosting': GradientBoostingRegressor(),
                 'Linear Regressor': LinearRegression(),
                 'K-Neighbors Regressor': KNeighborsRegressor(),
-                'XGBRegressor': XGBRegressor(),
-                'CatBoost Regressor': CatBoostRegressor(verbose=False),
+                # 'XGBRegressor': XGBRegressor(),
+                # 'CatBoost Regressor': CatBoostRegressor(verbose=False),
                 'AdaBoost Regressor': AdaBoostRegressor(),
             }
 
-            model_report: dict = evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models)
+            ## Hyperparameter tuning
+            params = {
+                "Decision Tree": {
+                    'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                },
+                "Random Forest": {
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "Gradient Boosting": {
+                    'learning_rate': [0.1, 0.01, 0.05, 0.001],
+                    'subsample': [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "Linear Regressor": {},
+                "K-Neighbors Regressor": {
+                    'n_neighbors': [3, 5, 7, 9],
+                    'weights': ['uniform', 'distance'],
+                    'algorithm': ['auto', 'ball_tree', 'kd_tree', 'brute']
+                },
+                # "XGBRegressor": {
+                #     'learning_rate': [0.1, 0.01, 0.05, 0.001],
+                #     'n_estimators': [8, 16, 32, 64, 128, 256]
+                # },
+                # "CatBoosting Regressor": {
+                #     'depth': [6, 8, 10],
+                #     'learning_rate': [0.01, 0.05, 0.1],
+                #     'iterations': [30, 50, 100]
+                # },
+                "AdaBoost Regressor": {
+                    'learning_rate': [0.1, 0.01, 0.5, 0.001],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                
+            }
+
+
+            model_report: dict = evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models, params = params)
 
             best_model_score = max(sorted(model_report.values()))
             best_model_name = list(model_report.keys())[list(model_report.values()).index(best_model_score)]
@@ -59,6 +95,7 @@ class ModelTrainer:
 
             if best_model_score < 0.6:
                 raise CustomException('No best model found')
+            
             logging.info(f'Best found model on both training and testing dataset')
 
             save_object(
--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,7 @@ import pandas as pd
 
 from sklearn.metrics import r2_score
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -22,7 +23,7 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     try:
         X_train, X_test, y_train, y_test = train_test_split(
             X_train, y_train, test_size=0.2, random_state=42
@@ -32,7 +33,12 @@ def evaluate_models(X_train, y_train, X_test, y_test, models):
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para=params[list(models.keys())[i]]
 
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)  #Train Model
 
             y_train_pred = model.predict(X_train)
